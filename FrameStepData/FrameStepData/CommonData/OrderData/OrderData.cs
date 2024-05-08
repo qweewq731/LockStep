2f738c0a8cfadc94128f6f72a176d3e02f065c37
Huyu <41512577+qweewq731@users.noreply.github.com>
2024-05-08 17:38:16 +0800

    1

---
+++ FrameStepData/FrameStepData/CommonData/OrderData/OrderData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPStepServer.Data
+{
+    /// <summary>
+    /// 考虑一下角色当期奥状态，如果没有输入，我们是沿用之前的指令集？
+    /// 使用密文状态去发送指令，每个指令其实是{Reciver,target,}
+    /// </summary>
+    public class OrderDataBase
+    {
+        //帧数 一秒30帧为例
+        //一分钟为1800帧
+        //一个小时为108000帧
+        //一天为2592000帧
+        public int tick { get; set; }
+        //当前玩家的指令
+        //操作不一定有接受者但是一定有操作的人
+        public int roleUID { get; set; } 
+    }
+}
---
+++ FrameStepData/FrameStepData/CommonData/OrderData/OrderMoveDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UDPStepServer.Data;
+
+namespace UDPStepServer
+{
+    internal class OrderMoveDirection : OrderDataBase
+    {
+
+    }
+}
